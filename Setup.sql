0ac9cd2553da5235e6fa2126df6b9df7e42e2dd5
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-08-16 11:06:55 +0200

    adding the sql schema file

---
+++ Setup.sql
@@ -0,0 +1,29 @@
+CREATE TABLE members (
+    id bigint NOT NULL PRIMARY KEY,
+    [name] varchar(255) NOT NULL,
+    [email] varchar(255) NOT NULL,
+    [start_date] datetime,
+    [leave_date] datetime,
+    [position] int NOT NULL FOREIGN KEY REFERENCES positions(id)
+);
+
+CREATE TABLE leaves (
+    id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [member_id] bigint NOT NULL FOREIGN KEY REFERENCES members(id),
+    [request_id] bigint NOT NULL,
+    [leave_type] int NOT NULL FOREIGN KEY REFERENCES leaveTypes(id),
+    [leave_status] varchar(50) NOT NULL,
+    [date] datetime NOT NULL,
+    [reason] varchar(255),
+    [remark] varchar(255)
+);
+
+CREATE TABLE positions (
+    id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [name] varchar(255) NOT NULL
+);
+
+CREATE TABLE leaveTypes (
+    id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [name] varchar(255) NOT NULL
+);
