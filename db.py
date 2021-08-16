71bd837e53d7c2638c0bbcef1b316dff8b884054
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-08-16 14:36:06 +0200

    added general helper function get caption to db file

--- db.py
+++ db.py
@@ -7,3 +7,21 @@ def load_db():
 
     conn = pyodbc.connect(os.getenv("Connection_String"))
     cursor = conn.cursor()
+
+def GetCaption(captionCode):
+    switcher = {
+        1: "Your leave request has been sent",
+        2: "You dont have enough leaves to request, your current balance is ",
+        3: "Please select valid dates",
+        4: "Your Request has failed, try again later",
+        5: "Your annual leave request was approved",
+        6: "Your annual leave request was rejected",
+        7: "Your request is being processed",
+        8: "It is past core hours your leave request with be considered as an emergency leave",
+        9: "Your emergency leave request was approved",
+        10: "Your emergency leave request was rejected",
+        11: "Your sick leave request was approved",
+        12: "Your sick leave request was rejected",
+    }
+
+    return switcher.get(captionCode, lambda: "Invalid caption code")
