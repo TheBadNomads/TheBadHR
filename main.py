c3e714d6bbfb6876e1dc6ffcd2e317411521a3d6
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-08-20 21:34:35 +0200

    adding leave commands

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from discord.ext import commands
 from discord_slash import SlashCommand
 from discord_components import DiscordComponents, Button, Select, SelectOption, message
 from Member import member_interface as mi
+from Leave import leave_interface as li
 
 load_dotenv()
 
@@ -23,16 +24,22 @@ async def on_ready():
 
 @client.event
 async def on_raw_reaction_add(payload):
-    await Leaves.HandleLeaveReactions(client, payload)
+    await li.HandleLeaveReactions(client, payload)
 
 @slash.slash(name = "RequestLeave", description = "Request an annual leave", options = UI.CreateDateOptions(), guild_ids = guild_ids)
 async def RequestLeave(ctx, leavetype, startdate, enddate):
-    await Leaves.RequestLeave(ctx, client, leavetype, startdate, enddate)
+    await li.RequestLeave(ctx, client, leavetype, startdate, enddate)
 
 @slash.slash(name = "InsertMember", description = "Insert new member into the database", options = UI.CreateMemberOptions(), guild_ids = guild_ids)
 async def InsertMember(ctx, discorduser, name, email, startdate, leavedate, position):
     result = mi.InsertMember(discorduser.id, name, email, startdate, leavedate, position)
     await ctx.send(content = "Success" if result else "Failed")
+
+@slash.slash(name = "CheckBalance", description = "Checks the available leave balance", options = UI.CreateBalanceOptions(), guild_ids = guild_ids)
+async def CheckBalance(ctx, leavetype):
+    result = li.GetLeaveBalance(ctx.author.id, leavetype)
+    await ctx.author.send(content = str(result))
+    await ctx.send(content = "Request was sent")
    
 
 client.run(os.getenv("Bot_token"))
