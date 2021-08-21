0a595babd45f9c0cf232b3479dcd4dcbed9c50e4
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-08-21 14:26:30 +0200

    moving get caption to db file

--- db.py
+++ db.py
@@ -24,4 +24,24 @@ class db:
             db.cursor = db.GetDBConnection().cursor()
 
         return db.cursor
+    
+    def GetCaption(captionCode):
+        captions = {
+            1: "Your leave request has been sent",
+            2: "You dont have enough leaves to request, your current balance is ",
+            3: "Please select valid dates",
+            4: "Your Request has failed, try again later",
+            5: "Your annual leave request was approved",
+            6: "Your annual leave request was rejected",
+            7: "Your request is being processed",
+            8: "It is past core hours your leave request with be considered as an emergency leave",
+            9: "Your emergency leave request was approved",
+            10: "Your emergency leave request was rejected",
+            11: "Your sick leave request was approved",
+            12: "Your sick leave request was rejected",
+        }
+
+        captions = defaultdict("Invalid caption code", **captions)
+
+        return captions[captionCode]
 
