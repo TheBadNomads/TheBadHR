0e50c50d68e4428f25d670aa1f8ca043dc54298b
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-08-22 13:09:36 +0200

    adjusting leave type table and relations

--- SQL_Setup_1.sql
+++ SQL_Setup_1.sql
@@ -1,6 +1,5 @@
 CREATE TABLE leaveTypes (
-    id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    [name] varchar(255) NOT NULL
+    [name] varchar(255) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE members (
@@ -15,7 +14,7 @@ CREATE TABLE leaves (
     id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
     [member_id] bigint NOT NULL FOREIGN KEY REFERENCES members(id),
     [request_id] bigint NOT NULL,
-    [leave_type] int NOT NULL FOREIGN KEY REFERENCES leaveTypes(id),
+    [leave_type] varchar(255) NOT NULL FOREIGN KEY REFERENCES leaveTypes(name),
     [leave_status] varchar(50) NOT NULL,
     [date] datetime NOT NULL,
     [reason] varchar(255),
@@ -25,11 +24,11 @@ CREATE TABLE leaves (
 CREATE TABLE leavesBalance (
     id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
     [member_id] bigint NOT NULL FOREIGN KEY REFERENCES members(id),
-    [leave_type] int NOT NULL FOREIGN KEY REFERENCES leaveTypes(id),
+    [leave_type] varchar(255) NOT NULL FOREIGN KEY REFERENCES leaveTypes(name),
     [balance] float NOT NULL
 );
 
 INSERT INTO leaveTypes (name) VALUES ('Annual');
-INSERT INTO leaveTypes VALUES ('Emergency');
-INSERT INTO leaveTypes VALUES ('Sick');
-INSERT INTO leaveTypes VALUES ('Unpaid');
+INSERT INTO leaveTypes (name) VALUES ('Emergency');
+INSERT INTO leaveTypes (name) VALUES ('Sick');
+INSERT INTO leaveTypes (name) VALUES ('Unpaid');
