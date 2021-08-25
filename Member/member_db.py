b91773235f46b53f9685c0391f0568e3c582f07f
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-08-25 20:34:56 +0200

    removing the interface file and adjusting the calling of the db functions

--- Member/member_db.py
+++ Member/member_db.py
@@ -1,40 +1,30 @@
 from db import db
 from datetime import datetime
 
-class member:
-    def __init__(self, attrs):
-        self.id = attrs[0]
-        self.name = attrs[1]
-        self.email = attrs[2]
-        self.start_date = attrs[3]
-
-    def GetMemberByID(id):
+def GetMemberByID(id):
         db.GetDBCursor().execute(f'SELECT * FROM [members] WHERE id = {id}')
         row = db.GetDBCursor().fetchone()
         
-        return member(row)
+        return row
     
-    def GetMembers():
-        db.GetDBCursor().execute(f'SELECT * FROM [members]')
-        rows = db.GetDBCursor().fetchall()
-        members = []
-        for row in rows:
-            members.append(member(row))
-        
-        return members
+def GetMembers():
+    db.GetDBCursor().execute(f'SELECT * FROM [members]')
+    rows = db.GetDBCursor().fetchall()
     
-    def InsertMember(id:int, name:str, email:str, start_date:datetime):
-        try:
-            db.GetDBCursor().execute(
-                "INSERT INTO [members] (id, name, email, start_date, leave_date) VALUES (?, ?, ?, ?, ?)",
-                (id, name, email, start_date, datetime.strptime('1/1/3000', '%m/%d/%Y'))
-            )    
-            db.GetDBConnection().commit()
-            # TODO: Insert Leave balance on success
+    return rows
 
-            return "Success"
+def InsertMember(id:int, name:str, email:str, start_date:datetime):
+    try:
+        db.GetDBCursor().execute(
+            "INSERT INTO [members] (id, name, email, start_date, leave_date) VALUES (?, ?, ?, ?, ?)",
+            (id, name, email, datetime.strptime(start_date, '%m/%d/%Y'), datetime.strptime('1/1/3000', '%m/%d/%Y'))
+        )    
+        db.GetDBConnection().commit()
+        # TODO: Insert Leave balance on success
 
-        except Exception as e:
-            db.GetDBConnection().rollback()
+        return "Success"
 
-            return "failed"
+    except Exception as e:
+        db.GetDBConnection().rollback()
+
+        return "failed"
