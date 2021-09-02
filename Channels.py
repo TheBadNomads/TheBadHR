2b7746af4520b1e14bad09066d4312af88235a08
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-09-02 12:19:30 +0200

    removing async and fetch calling

--- Channels.py
+++ Channels.py
@@ -5,8 +5,8 @@ class Channels:
     LeaveApprovalsChannel = None
 
     @staticmethod
-    async def GetLeaveApprovalsChannel(client):
+    def GetLeaveApprovalsChannel(client):
         if(Channels.LeaveApprovalsChannel == None):
-            Channels.LeaveApprovalsChannel = await client.fetch_channel(int(os.getenv("TestChannel_id")))
+            Channels.LeaveApprovalsChannel = client.get_channel(int(os.getenv("TestChannel_id")))
 
         return Channels.LeaveApprovalsChannel
