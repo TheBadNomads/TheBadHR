18556f5bf9066467c5a72584b7e2845f1409addc
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-09-02 12:03:29 +0200

    adding Crud ops for leaves type

--- Leave/leave_db.py
+++ Leave/leave_db.py
@@ -21,6 +21,13 @@ def GetLeaveStatus(request_id):
 
     return leave["leave_status"]
 
+def GetLeaveBalance(member_id, leave_type):
+    db.GetDBCursor().execute(f'SELECT balance FROM [leavesBalance] WHERE member_id = {member_id} AND leave_type = {leave_type}')
+    row = db.GetDBCursor().fetchone()
+    leaves_balance = utils.ConvertJsonToDic(row)
+
+    return float(leaves_balance["balance"])
+
 def InsertLeave(member_id:int, request_id:int, leave_type:int, date:datetime, reason:str, remark:str, leave_status:str):
     try:
         db.GetDBCursor().execute(
@@ -34,9 +41,38 @@ def InsertLeave(member_id:int, request_id:int, leave_type:int, date:datetime, re
         db.GetDBConnection().rollback()
         return "Failed"
 
+def InsertLeaveBalance(member_id:int, start_date:datetime):
+    try:
+        for leaveType in GetLeaveTypes():
+            try:
+                db.GetDBCursor().execute(
+                    "INSERT INTO [leavesBalance] (member_id, leave_type, balance) VALUES (?, ?, ?)",
+                    (member_id, leaveType["id"], utils.CalculateLeaveTypeBalance(leaveType["id"], start_date))
+                )
+            except Exception as e:
+                print(e)
+                return "Failed"
+
+        db.GetDBConnection().commit()
+        return "Success"
+
+    except Exception as e:
+        db.GetDBConnection().rollback()
+        return "Failed"
+
 def UpdateLeaveStatus(request_id, leave_status):
     try:
         db.GetDBCursor().execute("UPDATE [leaves] SET leave_status = ? WHERE request_id = ?", leave_status, request_id)
+        db.GetDBConnection().commit()
+        return "Success"
+
+    except Exception as e:
+        db.GetDBConnection().rollback()
+        return "Failed"
+
+def UpdateLeaveBalance(member_id, leave_type, requested_days):
+    try:
+        db.GetDBCursor().execute("UPDATE [leavesBalance] SET balance = balance + ? WHERE member_id = ? AND leave_type = ?", requested_days, member_id, leave_type)
         db.GetDBConnection().commit()
         return "Success"
 
