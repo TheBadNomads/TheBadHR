fce44acdaa70533e9fb6c6e89ac7c225691067f2
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-09-11 22:09:06 +0200

    adjusting the flow of leave request

--- Leave/leave_interface.py
+++ Leave/leave_interface.py
@@ -6,24 +6,35 @@ from Channels import Channels
 from datetime import datetime
 from db import db
 from Leave import leave_db
-from Member import member_db
 
 async def RequestLeave(ctx, member, client, leavetype, startdate, enddate, reason):
-    current_time = datetime.now().hour
     if utils.ValidateDates(startdate, enddate):
         if utils.CheckAvailableBalance(member, startdate, enddate, leavetype):
-            if current_time >= 12:
-                await WarnRequester(ctx, client, startdate, enddate, reason)
-            else:
-                await CompleteRequest(ctx, client, startdate, enddate, leavetype, reason)
-
+            await ProccessRequest(ctx, member, client, leavetype, startdate, enddate, reason)
         else:
             await ctx.send(content = db.GetCaption(2) + leave_db.GetLeaveBalance(member.id, leavetype))
 
     else:
         await ctx.send(content = db.GetCaption(3))
 
-async def WarnRequester(ctx, client, startdate, enddate, reason):
+async def ProccessRequest(ctx, member, client, startdate, enddate, leavetype, reason):
+    current_time = datetime.now().hour
+
+    if ctx.author.id != member.id:
+        await CompleteRequest(ctx, member, client, startdate, enddate, leavetype, reason)
+        return
+    
+    if current_time < 12:
+        await CompleteRequest(ctx, member, client, startdate, enddate, leavetype, reason)
+        return
+
+    if leavetype.lower() == "annual" or leavetype.lower() == "sick":
+        await WarnRequester(ctx, member, client, startdate, enddate, reason)
+        return
+
+    await CompleteRequest(ctx, member, client, startdate, enddate, leavetype, reason)
+
+async def WarnRequester(ctx, member, client, startdate, enddate, reason):
     await ctx.send(content = db.GetCaption(7))
     message = await ctx.author.send(embed = UI.CreateWarningEmbed())
     await message.add_reaction(os.getenv("Approve_Emoji"))
@@ -35,9 +46,9 @@ async def WarnRequester(ctx, client, startdate, enddate, reason):
     reaction = await client.wait_for('raw_reaction_add', check = check, timeout = 120.0)
 
     if str(reaction.emoji) == os.getenv("Approve_Emoji"):
-        await CompleteRequest(ctx, client, startdate, enddate, 2, reason)
+        await CompleteRequest(ctx, member, client, startdate, enddate, "Emergency", reason)
 
-async def CompleteRequest(ctx, client, startdate, enddate, leaveType, reason):
+async def CompleteRequest(ctx, member, client, startdate, enddate, leaveType, reason):
     await ctx.send(content = db.GetCaption(1))
     embed = UI.CreateLeaveEmbed(ctx, startdate, enddate, leaveType)
     channel = await Channels.GetLeaveApprovalsChannel(client)
@@ -45,11 +56,10 @@ async def CompleteRequest(ctx, client, startdate, enddate, leaveType, reason):
     await message.add_reaction(os.getenv("Approve_Emoji"))
     await message.add_reaction(os.getenv("Reject_Emoji"))
 
-    member = member_db.GetMemberByID(ctx.author.id)
     requested_days = utils.GetRequestedDays(startdate, enddate)
 
     for day in requested_days:
-        leave_db.InsertLeave(member["id"], message.id, leaveType, "pending", day, reason, "")
+        leave_db.InsertLeave(member.id, message.id, leaveType, "pending", day, reason, "")
 
 async def HandleLeaveReactions(client, payload):
     channel = client.get_channel(payload.channel_id)
