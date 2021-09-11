5574e27d8200c1b360633a159820738f5053d955
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-09-12 01:09:33 +0200

    fixing warning request issues (not finished)

--- Leave/leave_interface.py
+++ Leave/leave_interface.py
@@ -7,6 +7,20 @@ from datetime import datetime
 from db import db
 from Leave import leave_db
 
+# class TmpEmergancyRequest:
+#     def __init__(self, ctx, message_id, member, startdate, enddate, reason):
+#         self.ctx = ctx
+#         self.message_id = message_id
+#         self.member = member
+#         self.startdate = startdate
+#         self.enddate = enddate
+#         self.reason = reason
+    
+#     def __str__(self):
+#         return str(self.__class__) + ": " + str(self.__dict__)
+
+# tmpRequestsData = [TmpEmergancyRequest]
+
 async def RequestLeave(ctx, member, client, leavetype, startdate, enddate, reason):
     if utils.ValidateDates(startdate, enddate):
         if utils.CheckAvailableBalance(member, startdate, enddate, leavetype):
@@ -24,7 +38,7 @@ async def ProccessRequest(ctx, member, client, startdate, enddate, leavetype, re
         await CompleteRequest(ctx, member, client, startdate, enddate, leavetype, reason)
         return
     
-    if current_time < 12:
+    if current_time > 12:
         await CompleteRequest(ctx, member, client, startdate, enddate, leavetype, reason)
         return
 
@@ -40,13 +54,17 @@ async def WarnRequester(ctx, member, client, startdate, enddate, reason):
     await message.add_reaction(os.getenv("Approve_Emoji"))
     await message.add_reaction(os.getenv("Reject_Emoji"))
 
-    def check(reaction):
-        return (reaction.message_id == message.id) and (str(reaction.emoji) in [os.getenv("Approve_Emoji"), os.getenv("Reject_Emoji")])
+    # tmpData = TmpEmergancyRequest(ctx, message.id, member, startdate, enddate, reason)
+    # tmpRequestsData.append(tmpData)
+    # def check(reaction):
+    #     return (reaction.message_id == message.id) and (str(reaction.emoji) in [os.getenv("Approve_Emoji"), os.getenv("Reject_Emoji")])
 
-    reaction = await client.wait_for('raw_reaction_add', check = check, timeout = 120.0)
-
-    if str(reaction.emoji) == os.getenv("Approve_Emoji"):
-        await CompleteRequest(ctx, member, client, startdate, enddate, "Emergency", reason)
+    # reaction = await client.wait_for('raw_reaction_add', check = check, timeout = 120.0)
+    # if str(reaction.emoji) == os.getenv("Approve_Emoji"):
+    #     await CompleteRequest(ctx, member, client, startdate, enddate, "Emergency", reason)
+    #     return 
+    
+    # await message.delete()
 
 async def CompleteRequest(ctx, member, client, startdate, enddate, leaveType, reason):
     await ctx.send(content = db.GetCaption(1))
@@ -62,24 +80,55 @@ async def CompleteRequest(ctx, member, client, startdate, enddate, leaveType, re
         leave_db.InsertLeave(member.id, message.id, leaveType, "pending", day, reason, "")
 
 async def HandleLeaveReactions(client, payload):
+    if payload.guild_id != None:
+        await HandleNormalRequest(client, payload)
+    # else:
+    #     await HandleSpecialRequest(client, payload)
+
+# async def HandleSpecialRequest(client, payload):
+#     tmpReq = None
+
+#     for elem in tmpRequestsData:
+#         print(elem)
+#         if elem.message_id == payload.message_id:
+#             tmpReq = elem
+#             break
+
+#     print(tmpReq)
+
+#     if payload.member != None and utils.isNotBot(payload.member):
+#         if tmpReq != None:
+#             status = UI.ParseEmoji(payload.emoji)
+#             print(status)
+#             if status == "Approved":
+#                 await CompleteRequest(tmpReq.ctx, tmpReq.member, client, tmpReq.startdate, tmpReq.enddate, "Emergency", tmpReq.reason)
+            
+#             await tmpReq.message.delete()
+#             tmpRequestsData.remove(tmpReq)
+
+async def HandleNormalRequest(client, payload):
     channel = client.get_channel(payload.channel_id)
     message = await channel.fetch_message(payload.message_id)
     embed = message.embeds[0]
 
     if utils.isNotBot(payload.member) and utils.isLeaveRequest(payload.message_id) and utils.isPending(payload.message_id):
         status = UI.ParseEmoji(payload.emoji)
+        if status != None:
+            await UpdateLeaveStatus(payload, status, message, embed)
+            if status == "Approved":
+                UpdateLeaveBalance(payload)
 
-        if status != "":
-            try:
-                leave_db.UpdateLeaveStatus(payload.message_id, status)
-                await UI.UpdateEmbedLeaveStatus(message, embed, status)
-                await payload.member.send(content = "Your request was " + status)
+async def UpdateLeaveStatus(payload, status, message, embed):
+    try:
+        leave_db.UpdateLeaveStatus(payload.message_id, status)
+        await UI.UpdateEmbedLeaveStatus(message, embed, status)
+        await payload.member.send(content = "Your request was " + status)
 
-                if status == "Approved":
-                    leaves = leave_db.GetLeaveByRequestID(payload.message_id)
-                    leave = leaves[0]
-                    leave_db.UpdateLeaveBalance(payload.member.id, leave["leave_type"], -len(leaves))
+    except Exception as e:
+        print(e)
 
-            except Exception as e:
-                print(e)
-            
+def UpdateLeaveBalance(payload):
+    leaves = leave_db.GetLeaveByRequestID(payload.message_id)
+    leave = leaves[0]
+    leave_db.UpdateLeaveBalance(payload.member.id, leave["leave_type"], -len(leaves))
+                
