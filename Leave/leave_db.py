2db3e41144e47bbe248381107166292e6f5696fa
AbdoHamed-TheBadNomads <86910632+AbdoHamed-TheBadNomads@users.noreply.github.com>
2021-09-21 11:27:06 +0200

    Leave

--- Leave/leave_db.py
+++ Leave/leave_db.py
@@ -15,12 +15,28 @@ def GetLeavesByRequestID(request_id):
 
     return leaves
 
+def GetLeaveStatus(request_id):
+    db.GetDBCursor().execute(f'SELECT leave_status FROM [leaves] WHERE request_id = {request_id}')
+    leave = [dict(zip([column[0] for column in db.GetDBCursor().description], row)) for row in db.GetDBCursor().fetchall()][0]
+
+    return leave["leave_status"]
+
 def InsertLeave(member_id:int, request_id:int, leave_type:int, date:datetime, reason:str, remark:str, leave_status:str):
     try:
         db.GetDBCursor().execute(
             "INSERT INTO [leaves] (member_id, request_id, leave_type, date, reason, remark, leave_status) VALUES (?, ?, ?, ?, ?, ?, ?)",
             (member_id, request_id, leave_type, date, reason, remark, leave_status)
         )
+        db.GetDBConnection().commit()
+        return "Success"
+
+    except Exception as e:
+        db.GetDBConnection().rollback()
+        return "Failed"
+
+def UpdateLeaveStatus(request_id, leave_status):
+    try:
+        db.GetDBCursor().execute("UPDATE [leaves] SET leave_status = ? WHERE request_id = ?", leave_status, request_id)
         db.GetDBConnection().commit()
         return "Success"
 
