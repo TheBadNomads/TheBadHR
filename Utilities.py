336643a091bb324ebce24a38fe1bbd7294c80661
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-10-06 12:52:27 +0200

    removing the string to date convertion (not needed)

--- Utilities.py
+++ Utilities.py
@@ -20,30 +20,26 @@ def CalculateProrataForLeave(leave_type, start_date, starting_balance):
         return starting_balance
 
 def CalculateProratedAnnualLeaves(start_date, starting_balance):
-    start_month = int(datetime.strftime(start_date,"%m"))
+    start_month = start_date.month
     leaves_months_count = (12 - start_month) + 1
     leave_balance_per_month = starting_balance / 12
 
     return leaves_months_count * leave_balance_per_month
 
-def HasEnoughBalance(startdate: str, enddate: str, current_balance):
+def HasEnoughBalance(startdate, enddate, current_balance):
     requested_days_count = len(GetRequestedDays(startdate, enddate))
 
     return current_balance >= requested_days_count
 
-def GetRequestedDays(startdate: str, enddate: str):
-    sDate = datetime.strptime(startdate, '%m/%d/%Y')
-    eDate = datetime.strptime(enddate, '%m/%d/%Y')
+def GetRequestedDays(startdate, enddate):
     requested_days = []
-    for i in range((eDate - sDate).days + 1):
-        day = sDate + timedelta(days = i)
+    for i in range((enddate - startdate).days + 1):
+        day = startdate + timedelta(days = i)
         if (day.weekday() not in [4, 5]):
             requested_days.append(day) 
 
     return requested_days
 
-def IsDateOrderValid(startdate: str, enddate: str):
-    sDate = datetime.strptime(startdate, '%m/%d/%Y')
-    eDate = datetime.strptime(enddate, '%m/%d/%Y')
+def IsDateOrderValid(startdate, enddate):
 
-    return eDate >= sDate
+    return enddate >= startdate
