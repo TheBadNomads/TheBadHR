5132a906334e8632ae14f325e7838ef76037cbf6
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-10-27 11:09:08 +0200

    removing extra new lines

--- Utilities.py
+++ Utilities.py
@@ -11,7 +11,6 @@ def CalculateInitialLeavesBalance(leave_types_with_balance, start_date):
     calculated_leave_types_balance = {}
     for leave_type in leave_types_with_balance:
         calculated_leave_types_balance[leave_type["name"]] = CalculateProrataForLeave(leave_type["name"], start_date, leave_type["starting_balance"])
-    
     return calculated_leave_types_balance
 
 def CalculateProrataForLeave(leave_type, start_date, starting_balance):
@@ -25,12 +24,10 @@ def CalculateProratedAnnualLeaves(start_date, starting_balance):
     start_month = start_date.month
     leaves_months_count = (12 - start_month) + 1
     leave_balance_per_month = starting_balance / 12
-
     return leaves_months_count * leave_balance_per_month
 
 def HasEnoughBalance(startdate, enddate, current_balance):
     requested_days_count = len(GetRequestedDays(startdate, enddate))
-
     return current_balance >= requested_days_count
 
 def GetRequestedDays(startdate, enddate):
@@ -39,7 +36,6 @@ def GetRequestedDays(startdate, enddate):
         day = startdate + datetime.timedelta(days = i)
         if (day.weekday() not in [4, 5]):
             requested_days.append(day) 
-
     return requested_days
     
 def IsAdmin(member):
