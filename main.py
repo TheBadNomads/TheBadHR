3838cd7f727227035765e8981912a4bf17cdbc64
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-10-31 10:25:59 +0200

    renaming

--- main.py
+++ main.py
@@ -30,11 +30,11 @@ async def on_ready():
 async def on_raw_reaction_add(payload):
     await leave_interface.HandleLeaveReactions(client, payload)
 
-@slash.slash(name = "RequestLeave", description = "Request an annual leave", options = UI.CreateLeaveRequestOptions(), guild_ids = guild_ids)
+@slash.slash(name = "RequestLeave", description = "Requests an annual leave", options = UI.CreateLeaveRequestOptions(), guild_ids = guild_ids)
 async def RequestLeave(ctx, leavetype, startdate, enddate, reason = ""):
     await leave_interface.ProcessLeaveRequest(ctx, ctx.author, client, leavetype, datetime.strptime(startdate, '%d/%m/%Y'), datetime.strptime(enddate, '%d/%m/%Y'), reason)
 
-@slash.slash(name = "InsertMember", description = "Insert a new member into the database", options = UI.CreateMemberInsertionOptions(), guild_ids = guild_ids)
+@slash.slash(name = "InsertMember", description = "Inserts a new member into the database", options = UI.CreateMemberInsertionOptions(), guild_ids = guild_ids)
 async def InsertMember(ctx, discorduser, name, email, startdate):
     if Utilities.IsAdmin(ctx.author):
         result = member_db.InsertMember(discorduser.id, name, email, datetime.strptime(startdate, '%d/%m/%Y'))
@@ -42,7 +42,7 @@ async def InsertMember(ctx, discorduser, name, email, startdate):
     else:
         await ctx.send(content = "This command is for Admins only")
 
-@slash.slash(name = "InsertLateLeave", description = "Insert a late leave (Admins Only)", options = UI.CreateLateLeaveInsertionOptions(), guild_ids = guild_ids)
+@slash.slash(name = "InsertLateLeave", description = "Inserts a late leave (Admins Only)", options = UI.CreateLateLeaveInsertionOptions(), guild_ids = guild_ids)
 async def InsertLateLeave(ctx, discorduser, leavetype, startdate, enddate, reason = ""):
     if Utilities.IsAdmin(ctx.author):
         await leave_interface.InsertLateLeave(ctx, discorduser, datetime.strptime(startdate, '%d/%m/%Y'), datetime.strptime(enddate, '%d/%m/%Y'), leavetype, reason)
