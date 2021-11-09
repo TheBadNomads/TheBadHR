666f78a0212a6956ced9455997d0663355da5f38
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-11-09 12:46:21 +0200

    removing unpaid leave type

--- SQL_Setup_2.sql
+++ SQL_Setup_2.sql
@@ -1,6 +1,9 @@
 ALTER TABLE leaves
 ADD is_emergency BIT NOT NULL;
+ADD is_unpaid BIT NOT NULL;
 
 DELETE FROM leavesBalance WHERE leave_type = 'Emergency';
+DELETE FROM leavesBalance WHERE leave_type = 'Unpaid';
 
 DELETE FROM leaveTypesWithBalance WHERE name = 'Emergency';
+DELETE FROM leaveTypesWithBalance WHERE name = 'Unpaid';
