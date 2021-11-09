cfa5420cf034effc5d95f706b905d9bf21b2f416
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-11-09 12:53:56 +0200

    adding is unpaid param

--- Leave/leave_db.py
+++ Leave/leave_db.py
@@ -38,11 +38,11 @@ def GetLeaveTypesWithBalance():
     leaves_types = [dict(zip([column[0] for column in db.GetDBCursor().description], row)) for row in db.GetDBCursor().fetchall()]
     return leaves_types
 
-def InsertLeave(member_id, request_id, leave_type, date, reason, remark, leave_status, is_emergency):
+def InsertLeave(member_id, request_id, leave_type, date, reason, remark, leave_status, is_emergency, is_unpaid):
     try:
         db.GetDBCursor().execute(
-            "INSERT INTO [leaves] (member_id, request_id, leave_type, date, reason, remark, leave_status, is_emergency) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-            (member_id, request_id, leave_type, date, reason, remark, leave_status, is_emergency)
+            "INSERT INTO [leaves] (member_id, request_id, leave_type, date, reason, remark, leave_status, is_emergency) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (member_id, request_id, leave_type, date, reason, remark, leave_status, is_emergency, is_unpaid)
         )
         db.GetDBConnection().commit()
         return "Success"
