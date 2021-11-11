7f46e6ffd84c9359425659298e12afbca1c31827
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-11-11 12:20:19 +0200

    adjusting the flow of filtering rejected leaves

--- Leave/leave_interface.py
+++ Leave/leave_interface.py
@@ -15,9 +15,11 @@ async def ProcessLeaveRequest(ctx, member, client, leave_type, start_date, end_d
     if (len(utils.GetWorkDays(start_date, end_date)) <= 0):
         return ("This request consists of Holidays/Weekends ONLY")
 
-    repeated_requested_days = utils.ConvertDatesToStrings(GetRequestedDaysBetween(member.id, start_date, end_date))
-    if len(repeated_requested_days) > 0:
-        return (f"Leave request already exists for {repeated_requested_days}")
+    repeated_requested_leaves = GetRequestedLeavesBetween(member.id, start_date, end_date)
+    filtered_requested_days = [d['date'] for d in utils.FilterOutLeavesByStatus(repeated_requested_leaves, "rejected")]
+    previously_requested_days = utils.ConvertDatesToStrings(filtered_requested_days)
+    if len(previously_requested_days) > 0:
+        return (f"Leave request already exists for {previously_requested_days}")
     
     return await SubmitRequest(ctx, member, client, start_date, end_date, leave_type, reason)            
 
@@ -86,12 +88,14 @@ def UpdateLeaveBalanceOfRequestID(message_id):
     for leaves_array in list(ordered_requested_leaves.values()):
         leave_db.UpdateMultipleLeavesBalance(leaves_array)
 
-def GetRequestedDaysBetween(member_id, start_date, end_date):
+def GetRequestedLeavesBetween(member_id, start_date, end_date):
     work_days = utils.GetWorkDays(start_date, end_date)
     previous_leaves = leave_db.GetLeavesByMemberID(member_id)
-    previously_requested_days = [d['date'] for d in utils.FilterOutLeavesByStatus(previous_leaves, "rejected")]
-    requested_days = set(work_days).intersection(previously_requested_days)
-    return requested_days
+    requested_leaves = []
+    for leave in previous_leaves:
+        if leave["date"] in work_days:
+           requested_leaves.append(leave)
+    return requested_leaves
 
 def GetRemainingEmergencyLeavesCount(member_id):
     requested_emergency_count = len(leave_db.GetEmergencyLeavesForYear(member_id, datetime.date.today().year))
