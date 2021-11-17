912b4e5c742a37a6312f40a613df1db0cf8a4d5a
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-11-17 12:47:17 +0200

    using token to access private repos

--- start_bot.bat
+++ start_bot.bat
@@ -1,14 +1,14 @@
 @echo off
 set Owner=AbdoHamed-TheBadNomads
-set Repo=Abdo_Test_Repo_public
+set Repo=Abdo_Test_Repo
 set filename=Abdo.zip
 set OutputDir=%CD%
 
-curl https://api.github.com/repos/%Owner%/%Repo%/releases/latest > response.txt
+curl -H "Authorization: token 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" https://api.github.com/repos/%Owner%/%Repo%/releases/latest > response.txt
 FOR /F "tokens=*" %%g IN ('FIND "tag_name" "response.txt"') do (SET result=%%g)
 set "tag_name=%result:"tag_name": "=%"
 set "tag_name=%tag_name:",=%"
-curl -L -o %filename% https://github.com/%Owner%/%Repo%/archive/%tag_name%/%filename%
+curl -H "Authorization: token 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" -L -o %filename% https://github.com/%Owner%/%Repo%/archive/%tag_name%/%filename%
 "C:\Program Files\7-Zip\7z.exe" e %filename% -o%OutputDir% -y
 del /f %filename%
 del /f response.txt
