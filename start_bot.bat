e777e023ca4c5c14fd391771e4ced0c252f8f62e
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-11-17 13:06:03 +0200

    separeting token to separet var

--- start_bot.bat
+++ start_bot.bat
@@ -1,14 +1,15 @@
 @echo off
-set Owner=TheBadNomads
-set Repo=TheBadHR
+set owner=TheBadNomads
+set repo=TheBadHR
 set filename=latest.zip
-set OutputDir=%CD%
+set outputDir=%CD%
+set token=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
 
-curl -H "Authorization: token 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" https://api.github.com/repos/%Owner%/%Repo%/releases/latest > response.txt
+curl -H "Authorization: token %token%" https://api.github.com/repos/%owner%/%repo%/releases/latest > response.txt
 FOR /F "tokens=*" %%g IN ('FIND "tag_name" "response.txt"') do (SET result=%%g)
 set "tag_name=%result:"tag_name": "=%"
 set "tag_name=%tag_name:",=%"
-curl -H "Authorization: token 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" -L -o %filename% https://github.com/%Owner%/%Repo%/archive/%tag_name%/%filename%
-"C:\Program Files\7-Zip\7z.exe" e %filename% -o%OutputDir% -y
+curl -H "Authorization: token %token%" -L -o %filename% https://github.com/%owner%/%repo%/archive/%tag_name%/%filename%
+"C:\Program Files\7-Zip\7z.exe" e %filename% -o%outputDir% -y
 del /f %filename%
 del /f response.txt
