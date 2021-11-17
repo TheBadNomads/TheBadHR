03bf5db3bd092c020c6095b7f9e5cd06a2c12ec7
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-11-17 10:58:57 +0200

    first update process prototype

--- start_bot.bat
+++ start_bot.bat
@@ -1,4 +1,13 @@
-@echo
+@echo off
+set Owner=AbdoHamed-TheBadNomads
+set Repo=Abdo_Test_Repo_public
+set filename=Abdo.zip
+set OutputDir=%CD%
 
-git pull origin
-py -3 main.py
+curl https://api.github.com/repos/AbdoHamed-TheBadNomads/Abdo_test_repo_public/releases/latest > response.txt
+FOR /F "tokens=*" %%g IN ('FIND "tag_name" "response.txt"') do (SET result=%%g)
+set "tag_name=%VAR:"tag_name": "=%"
+set "tag_name=%tag_name:",=%"
+curl -L -o %filename% https://github.com/%Owner%/%Repo%/archive/%tag_name%/%filename%
+"C:\Program Files\7-Zip\7z.exe" e %filename% -o%OutputDir% -y
+del /f %filename%
