e411a748498ff17bfaa3fcc4aece88ae138bbdaf
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-11-18 11:07:04 +0200

    code refining

--- start_bot.bat
+++ start_bot.bat
@@ -7,7 +7,7 @@ set currentDir=%CD%
 set token=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
 
 curl -H "Authorization: token %token%" https://api.github.com/repos/%Owner%/%Repo%/releases/latest > response.txt
-FOR /F "tokens=*" %%g IN ('FIND "tag_name" "response.txt"') do (SET result=%%g)
+FOR /F "tokens=*" %%g IN ('FIND "tag_name" "response.txt"') do set result=%%g
 set "tag_name=%result:"tag_name": "=%"
 set "tag_name=%tag_name:",=%"
 curl -H "Authorization: token %token%" -L -o %filename% https://github.com/%Owner%/%Repo%/archive/%tag_name%/%filename%
