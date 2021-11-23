a298d1d121275a5399b6482b5ecbbe62013b6ceb
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-11-23 09:23:33 +0200

    adding the bat file

---
+++ Update_and_Start_Bot.bat
@@ -0,0 +1,15 @@
+@echo off
+set owner=TheBadNomads
+set repo=TheBadHR
+set filename=latest.zip
+set outputDir=%CD%
+set token=XXXXXX
+
+curl -H "Authorization: token %token%" https://api.github.com/repos/%owner%/%repo%/releases/latest > response.txt
+FOR /F "tokens=*" %%g IN ('FIND "tag_name" "response.txt"') do (SET result=%%g)
+set "tag_name=%result:"tag_name": "=%"
+set "tag_name=%tag_name:",=%"
+curl -H "Authorization: token %token%" -L -o %filename% https://github.com/%owner%/%repo%/archive/%tag_name%/%filename%
+"C:\Program Files\7-Zip\7z.exe" e %filename% -o%outputDir% -y
+del /f %filename%
+del /f response.txt
