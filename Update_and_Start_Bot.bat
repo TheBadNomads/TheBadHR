3cc5608a56c224c2db377dbbf312da2df3fb0e34
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-11-23 12:57:28 +0200

    removing the token to access private repo

--- Update_and_Start_Bot.bat
+++ Update_and_Start_Bot.bat
@@ -5,15 +5,12 @@ set filename=latest.zip
 set outputDir="C:\bot_latest_tmp"
 set currentDir=%CD%
 
-FOR /F "tokens=*" %%t IN ('FIND "Personal_Access_Token" ".env"') do (SET token_result=%%t)
-set "token=%token_result:~24%"
-
-curl -H "Authorization: token %token%" https://api.github.com/repos/%Owner%/%Repo%/releases/latest > response.txt
+curl https://api.github.com/repos/%Owner%/%Repo%/releases/latest > response.txt
 FOR /F "tokens=*" %%g IN ('FIND "tag_name" "response.txt"') do set result=%%g
 set "tag_name=%result:"tag_name": "=%"
 set "tag_name=%tag_name:",=%"
 
-curl -H "Authorization: token %token%" -L -o %filename% https://github.com/%Owner%/%Repo%/archive/%tag_name%/%filename%
+curl -L -o %filename% https://github.com/%Owner%/%Repo%/archive/%tag_name%/%filename%
 mkdir %OutputDir%
 tar -zxvf %filename% -C %OutputDir%
 for /D %%x in (%OutputDir%\*) do set newOutDir=%%x
