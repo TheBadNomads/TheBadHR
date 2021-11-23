3ba576ef4464bb5d8a638bec616c71615f3d6c14
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-11-23 09:43:18 +0200

    adjusting the update process

--- Update_and_Start_Bot.bat
+++ Update_and_Start_Bot.bat
@@ -8,12 +8,18 @@ FOR /F "tokens=*" %%t IN ('FIND "Personal_Access_Token" ".env"') do (SET token_r
 set "token=%token_result:~24%"
 echo %token%
 
-curl -H "Authorization: token %token%" https://api.github.com/repos/%owner%/%repo%/releases/latest > response.txt
-FOR /F "tokens=*" %%g IN ('FIND "tag_name" "response.txt"') do (SET result=%%g)
+curl -H "Authorization: token %token%" https://api.github.com/repos/%Owner%/%Repo%/releases/latest > response.txt
+FOR /F "tokens=*" %%g IN ('FIND "tag_name" "response.txt"') do set result=%%g
 set "tag_name=%result:"tag_name": "=%"
 set "tag_name=%tag_name:",=%"
 
-curl -H "Authorization: token %token%" -L -o %filename% https://github.com/%owner%/%repo%/archive/%tag_name%/%filename%
-"C:\Program Files\7-Zip\7z.exe" e %filename% -o%outputDir% -y
+curl -H "Authorization: token %token%" -L -o %filename% https://github.com/%Owner%/%Repo%/archive/%tag_name%/%filename%
+mkdir %OutputDir%
+tar -zxvf %filename% -C %OutputDir%
+for /D %%x in (%OutputDir%\*) do set newOutDir=%%x
+Xcopy %newOutDir% %currentDir% /E /H /C /I /Y
 del /f %filename%
 del /f response.txt
+rmdir /s /q %OutputDir%
+
+py -3 main.py
