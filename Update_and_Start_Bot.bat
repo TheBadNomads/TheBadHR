0804e979f50bba29b76a476421649bb1aaa9e297
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-11-23 09:40:24 +0200

    getting token from env file

--- Update_and_Start_Bot.bat
+++ Update_and_Start_Bot.bat
@@ -3,12 +3,16 @@ set owner=TheBadNomads
 set repo=TheBadHR
 set filename=latest.zip
 set outputDir=%CD%
-set token=XXXXXX
+
+FOR /F "tokens=*" %%t IN ('FIND "Personal_Access_Token" ".env"') do (SET token_result=%%t)
+set "token=%token_result:~24%"
+echo %token%
 
 curl -H "Authorization: token %token%" https://api.github.com/repos/%owner%/%repo%/releases/latest > response.txt
 FOR /F "tokens=*" %%g IN ('FIND "tag_name" "response.txt"') do (SET result=%%g)
 set "tag_name=%result:"tag_name": "=%"
 set "tag_name=%tag_name:",=%"
+
 curl -H "Authorization: token %token%" -L -o %filename% https://github.com/%owner%/%repo%/archive/%tag_name%/%filename%
 "C:\Program Files\7-Zip\7z.exe" e %filename% -o%outputDir% -y
 del /f %filename%
