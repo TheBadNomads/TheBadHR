991563702030d6e97c7ccca0ce08566b7fea6b99
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-11-27 14:43:48 +0200

    renaming

--- Leave/leave_db.py
+++ Leave/leave_db.py
@@ -35,8 +35,8 @@ def GetLeaveBalance(member_id, leave_type):
     db.GetDBCursor().execute(f"SELECT SUM(extra_balance) FROM extraBalance WHERE member_id = {member_id} AND leave_type = '{leave_type}'")
     extra_balance =  db.GetDBCursor().fetchone()[0]
     db.GetDBCursor().execute(f"SELECT COUNT(*) FROM leaves WHERE member_id = {member_id} AND leave_type = '{leave_type}'")
-    applied_leaves_count = db.GetDBCursor().fetchone()[0]
-    current_balance = (initial_balance + extra_balance) - applied_leaves_count
+    used_balance = db.GetDBCursor().fetchone()[0]
+    current_balance = (initial_balance + extra_balance) - used_balance
     return float(current_balance)
 
 def GetLeaveTypes():
