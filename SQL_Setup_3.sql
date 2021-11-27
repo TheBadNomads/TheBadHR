01417712749d40d7a6f9cac71ce0b835754d3960
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-11-27 14:00:25 +0200

    adding extra balance table

--- SQL_Setup_3.sql
+++ SQL_Setup_3.sql
@@ -6,3 +6,13 @@ DROP COLUMN starting_balance;
 DELETE FROM leavesBalance WHERE leave_type = 'Sick';
 
 DROP TABLE leavesBalance
+
+CREATE TABLE extraBalance (
+    id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [creditor_id] bigint NOT NULL FOREIGN KEY REFERENCES members(id),
+    [receiver_id] bigint NOT NULL,
+    [leave_type] varchar(255) NOT NULL FOREIGN KEY REFERENCES leaveTypes(name),
+    [date] datetime NOT NULL,
+    [reason] varchar(255),
+    [extra_balance] float NOT NULL
+);
