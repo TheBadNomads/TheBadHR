23b0150a4ac814904b957db694ad782046dba9eb
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-11-30 12:04:00 +0200

    removing redundant sql query

--- SQL_Setup_3.sql
+++ SQL_Setup_3.sql
@@ -3,8 +3,6 @@ sp_rename 'leaveTypesWithBalance', 'leaveTypes'
 ALTER TABLE leaveTypes
 DROP COLUMN starting_balance;
 
-DELETE FROM leavesBalance WHERE leave_type = 'Sick';
-
 DROP TABLE leavesBalance
 
 CREATE TABLE extraBalance (
