1ff2446cbb61f87ee3ed6d7fcc9b4e9fa6efb1c1
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-12-14 11:38:38 +0200

    adding new utils functions

--- Utilities.py
+++ Utilities.py
@@ -1,7 +1,9 @@
 import discord
 import os
 import datetime
-import re 
+import re
+
+from calendar import monthrange
 
 def isNotBot(member):
     return not member.bot
@@ -60,3 +62,9 @@ def IsUnpaidLeave(leave_type, leave_balance, is_emergency, remaining_emergency_c
 
 def GetDatesOfLeaves(leaves_array):
     return ([d['date'] for d in leaves_array])
+
+def GetMonthDaysCount(month, year):
+    return monthrange(year, month)[1]
+
+def CalculatePercentage(total, acctual):
+    return ((acctual * 100) / total)
