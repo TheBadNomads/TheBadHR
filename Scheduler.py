1e9536652a8e8c324945c895a56eb8576e5ae977
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-12-22 12:25:22 +0200

    structure refining

---
+++ Scheduler.py
@@ -0,0 +1,25 @@
+from calendar import c
+import os
+import UI
+
+from datetime import datetime
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+def Setup(client):
+    global bot
+    bot = client
+    scheduler = AsyncIOScheduler(timezone = "est")
+    AddSchedulerJobs(scheduler)
+    scheduler.start()
+
+def AddSchedulerJobs(scheduler):
+    scheduler.add_job(SendEndOfMonthCalculations, CronTrigger(day = 22, hour = 12, minute = 24))
+    # add more jobs here
+
+async def SendEndOfMonthCalculations(finance_admin = None, month = None, year = None):
+    month = month or datetime.now().month
+    year = year or datetime.now().year
+    finance_admin = finance_admin or await bot.fetch_user(int(os.getenv("Finance_Admin_id")))
+    embed = UI.CreateGetEndOfMonthCalculationsEmbed(month, year)
+    await finance_admin.send(embed = embed)
