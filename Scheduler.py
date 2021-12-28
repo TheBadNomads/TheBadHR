6a29cf828cf945db2696dddfe0b3e0763432dca6
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-12-28 13:14:42 +0200

    code refining

--- Scheduler.py
+++ Scheduler.py
@@ -4,6 +4,7 @@ import UI
 from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
+from Member import member_db 
 
 def Setup(client):
     global bot
@@ -13,16 +14,16 @@ def Setup(client):
     scheduler.start()
 
 def AddSchedulerJobs(scheduler):
-    scheduler.add_job(GetEndofMonthReport, "cron", day = int(os.getenv("End_of_Month_Report_Day")), hour = int(os.getenv("End_of_Month_Report_hour")), minute = int(os.getenv("End_of_Month_Report_minute")))
-    scheduler.add_job(GetEndofYearReport, "cron", month = int(os.getenv("End_of_Year_Report_Month")), day = int(os.getenv("End_of_Year_Report_Day")), hour = int(os.getenv("End_of_Year_Report_hour")), minute = int(os.getenv("End_of_Year_Report_minute")))
+    scheduler.add_job(GetEndofMonthReport, "cron", day = int(os.getenv("End_of_Month_Report_Day")), hour = int(os.getenv("End_of_Month_Report_Hour")), minute = int(os.getenv("End_of_Month_Report_Minute")))
+    scheduler.add_job(GetEndofYearReport, "cron", month = int(os.getenv("End_of_Year_Report_Month")), day = int(os.getenv("End_of_Year_Report_Day")), hour = int(os.getenv("End_of_Year_Report_Hour")), minute = int(os.getenv("End_of_Year_Report_Minute")))
     # add more jobs here
 
 async def GetEndofMonthReport():
     finance_admin = await bot.fetch_user(int(os.getenv("Finance_Admin_id")))
-    embed = UI.CreateGetEndOfMonthReportEmbed()
+    embed = UI.CreateGetEndOfMonthReportEmbed(member_db.GetMembers())
     await finance_admin.send(embed = embed)
 
 async def GetEndofYearReport():
     finance_admin = await bot.fetch_user(int(os.getenv("Finance_Admin_id")))
-    embed = UI.CreateGetEndOfYearReportEmbed()
+    embed = UI.CreateGetEndOfYearReportEmbed(member_db.GetMembers())
     await finance_admin.send(embed = embed)
