70aceac8fb64ce3b7f49829501d93d77b507ea82
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2021-12-28 11:58:41 +0200

    code refining

--- UI.py
+++ UI.py
@@ -327,9 +327,9 @@ def FormatGetEndOfYearReportEmbed(member, year):
     remaining_leaves_balance = leave_db.GetAnnualLeaveBalance(member["id"])
     bonus_precentage = utils.CalculatePercentage(float(os.getenv("Average_Working_Days_Count")), remaining_leaves_balance)
 
+    member_data += f' \u200B \u200B ***Paid Leaves Taken:*** \u200B \u200B{len(paid_leaves)} \u200B \u200B ***Sick:*** {len(sick_leaves)} \u200B \u200B ***Emergency:*** {len(emergency_leaves)}\n'
     member_data += f' \u200B \u200B ***Remaining Leaves Balance:*** \u200B \u200B{remaining_leaves_balance}\n'
     member_data += f' \u200B \u200B ***Bonus Percentage:*** \u200B \u200B{bonus_precentage}%\n'
-    member_data += f' \u200B \u200B ***Paid Leaves Taken:*** \u200B \u200B{len(paid_leaves)} \u200B \u200B ***Sick:*** {len(sick_leaves)} \u200B \u200B ***Emergency:*** {len(emergency_leaves)}\n'
         
     return member_data
 
