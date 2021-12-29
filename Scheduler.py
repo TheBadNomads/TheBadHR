25a3b4e0c580354dccd25360c973ed3c26c23c20
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2021-12-30 00:22:41 +0200

    renmaing of the functions

--- Scheduler.py
+++ Scheduler.py
@@ -13,17 +13,17 @@ def Setup(client):
     scheduler.start()
 
 def AddSchedulerJobs(scheduler):
-    scheduler.add_job(GetEndofMonthReport, "cron", day = int(os.getenv("End_of_Month_Report_Day")), hour = int(os.getenv("End_of_Month_Report_Hour")), minute = int(os.getenv("End_of_Month_Report_Minute")))
-    scheduler.add_job(GetEndofYearReport, "cron", month = int(os.getenv("End_of_Year_Report_Month")), day = int(os.getenv("End_of_Year_Report_Day")), hour = int(os.getenv("End_of_Year_Report_Hour")), minute = int(os.getenv("End_of_Year_Report_Minute")))
+    scheduler.add_job(SendEndofMonthReport, "cron", day = int(os.getenv("End_of_Month_Report_Day")), hour = int(os.getenv("End_of_Month_Report_Hour")), minute = int(os.getenv("End_of_Month_Report_Minute")))
+    scheduler.add_job(SendEndofYearReport, "cron", month = int(os.getenv("End_of_Year_Report_Month")), day = int(os.getenv("End_of_Year_Report_Day")), hour = int(os.getenv("End_of_Year_Report_Hour")), minute = int(os.getenv("End_of_Year_Report_Minute")))
 
-async def GetEndofMonthReport():
+async def SendEndofMonthReport():
     finance_admins_ids = os.getenv("Finance_Admins_ids").split(", ")
     embed = UI.CreateGetEndOfMonthReportEmbed(member_db.GetMembers())
     for id in finance_admins_ids:
         admin = await bot.fetch_user(int(id))
         await admin.send(embed = embed)
 
-async def GetEndofYearReport():
+async def SendEndofYearReport():
     finance_admins_ids = os.getenv("Finance_Admins_ids").split(", ")
     embed = UI.CreateGetEndOfYearReportEmbed(member_db.GetMembers())
     for id in finance_admins_ids:
