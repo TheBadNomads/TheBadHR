19059c64d13b40fcfc3035129491d1f1d9f189fa
AbdoHamed-TheBadNomads <86910632+AbdoHamed-TheBadNomads@users.noreply.github.com>
2021-12-30 20:50:10 +0200

    End_of_Month_Bug_Fix

--- UI.py
+++ UI.py
@@ -374,9 +374,8 @@ def CreateGetEndOfMonthReportEmbed(members_list, month = None, year = None):
 
 def FormatGetEndOfMonthReportEmbed(member, month, year):
     member_data = ""
-    previous_month = 12 if month == 1 else month - 1
-    start_date = datetime.datetime(year, previous_month, int(os.getenv("End_of_Month_Report_Day")))
-    end_date = datetime.datetime(year, month, int(os.getenv("End_of_Month_Report_Day")) - 1)
+    start_date = datetime.datetime(year, month, int(os.getenv("End_of_Month_Report_Day"))) - relativedelta(months = 1)
+    end_date = datetime.datetime(year, month, int(os.getenv("End_of_Month_Report_Day"))) - datetime.timedelta(days = 1)
     
     paid_leaves = leave_db.GetApprovedPaidLeaves(member["id"], start_date, end_date)
     unpaid_leaves = leave_db.GetApprovedUnpaidLeaves(member["id"], start_date, end_date)
