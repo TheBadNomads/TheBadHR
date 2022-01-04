fe80aea6bfe34bfea19aa05868e90a2963d4ea5b
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2022-01-04 13:03:05 +0200

    logic modifications

--- Pre_System_Inerstion.py
+++ Pre_System_Inerstion.py
@@ -10,15 +10,8 @@ data = pd.read_excel (os.getenv("Pre_System_Leaves_excel_Path"))
 data_dic = data.T.to_dict().values()
 
 for record in data_dic:
-    member_leaves_dates = Utilities.GetDatesOfLeaves(leave_db.GetLeavesByMemberID(record["Member_ID"]))
-    first_date = member_db.GetMemberByID(record["Member_ID"])["start_date"]
-    counter = 0
+    date = datetime(2021, 12, 1)
     days_count = record["Days_Count"]
     automatic_leave_id = -1
-    while counter < days_count:
-        date = first_date + timedelta(days = counter)
-        counter += 1
-        if(date in member_leaves_dates):
-            days_count += 1
-            continue
+    for counter in range(days_count):
         leave_db.InsertLeave(record["Member_ID"], automatic_leave_id, record["Leave_Type"], date, "", "", "Approved", record["Is_Emergency"], record["Is_Unpaid"])
