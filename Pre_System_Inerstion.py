a4fe591125dfeffb1a7a30b1b6f5e815b549ca9f
AbdoHamed-TheBadNomads <AbdelrahmanHamed@thebadnomads.com>
2022-01-04 11:20:43 +0200

    minor changes

--- Pre_System_Inerstion.py
+++ Pre_System_Inerstion.py
@@ -3,7 +3,7 @@ import Utilities
 import pandas as pd
 
 from Member import member_db 
-from Leave import leave_interface, leave_db
+from Leave import leave_db
 
 data = pd.read_excel (r'Leaves_Modified.xlsx')
 data_dic = data.T.to_dict().values()
@@ -13,10 +13,11 @@ for record in data_dic:
     first_date = member_db.GetMemberByID(record["Member_ID"])["start_date"]
     counter = 0
     days_count = record["Days_Count"]
+    automatic_leave_id = -1
     while counter < days_count:
         date = first_date + timedelta(days = counter)
         counter += 1
         if(date in member_leaves_dates):
             days_count += 1
             continue
-        print(leave_db.InsertLeave(record["Member_ID"], -1, record["Leave_Type"], date, "", "", "Approved", record["Is_Emergency"], record["Is_Unpaid"]))
+        leave_db.InsertLeave(record["Member_ID"], automatic_leave_id, record["Leave_Type"], date, "", "", "Approved", record["Is_Emergency"], record["Is_Unpaid"])
