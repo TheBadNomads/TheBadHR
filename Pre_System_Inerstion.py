55797ef84db73d553b51c14ef9fe916e7ccac2f8
AbdoHamed-TheBadNomads <abdelrahmanhamed@thebadnomads.com>
2022-01-09 18:28:21 +0200

    changed way of reading the csv file

--- Pre_System_Inerstion.py
+++ Pre_System_Inerstion.py
@@ -1,11 +1,9 @@
-import pandas as pd
+import csv
 
 from datetime import datetime
 from Leave import leave_db, leave_interface
 
 path = input("Please enter the path of your csv file:\n")
-data = pd.read_csv(path)
-data_dic = data.iterrows()
 members_dic = {
     "Ant"     : 158010930200838144,
     "Amr"     : 487749805636976651,
@@ -22,20 +20,25 @@ leave_types = ["Emergency Leave", "Leave"]
 date = datetime(2021, 12, 1)
 automatic_leave_id = -1
 is_emergency = False
-for record in data_dic:
-    if record[0] == "":
-        continue
 
-    if record[0] == "Total Leaves":
-        break
-    
-    if record[0] in leave_types:
-        is_emergency = False
-        if record[0] == "Emergency Leave":
-            is_emergency = True
+with open(path, "r") as f:
+    data = csv.reader(f, delimiter = "\t")
+    for line in data:
+        cols = line[0].split(',')
+        header_name = cols[0]
+        if header_name == "":
+            continue
 
-    if record[0] in members_dic:
-        days_count = record[11]
-        member_id = members_dic[record[0]]
-        for counter in range(days_count):
-            leave_interface.AddRetroactiveLeaveToDB(member_id, automatic_leave_id, date, date, "Annual", "Approved", "", is_emergency, None)
+        if header_name == "Total Leaves":
+            break
+        
+        if header_name in leave_types:
+            is_emergency = False
+            if header_name == "Emergency Leave":
+                is_emergency = True
+
+        if header_name in members_dic:
+            days_count = int(cols[11])
+            member_id = members_dic[header_name]
+            for counter in range(days_count):
+                leave_interface.AddRetroactiveLeaveToDB(member_id, automatic_leave_id, date, date, "Annual", "Approved", "", is_emergency, None)
